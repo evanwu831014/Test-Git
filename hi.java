6b36322df4a48f090f533165ec32c47e219fadb3
Evan Wu <evanwu831014@yahoo.com.tw>
2021-06-28 19:03:12 +0800

    add map str str

--- hi.java
+++ hi.java
@@ -1,8 +1,11 @@
 
 public class hi {
+private Map<String, String> word = new HashMap<String, String>();
+
 
 	public static void main(String[] args) {
 		String a ="222";
+		
 		System.out.println(123);
 		
 	}
